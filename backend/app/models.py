a6d5dc6cf0fe975636373de9a4a84fca592f29c0
binminh01 <nguyennhatminhbricsmath@gmail.com>
2026-02-05 22:23:31 +0700

    test image

--- backend/app/models.py
+++ backend/app/models.py
@@ -42,7 +42,7 @@ class Story(Base):
     id = Column(Integer, primary_key=True, index=True)
     slug = Column(String(100), unique=True, index=True, nullable=False)
     title = Column(String(200), nullable=False)
-    thumbnail_url = Column(String, nullable=True)
+    # thumbnail_url = Column(String, nullable=True)
     description = Column(Text)
     icon = Column(String(10))
     color = Column(String(100))
